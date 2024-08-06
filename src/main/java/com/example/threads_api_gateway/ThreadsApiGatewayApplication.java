833c91b843fb744a192ef023ceb7776be3663421
imran <imran@imrans-MacBook-Air.local>
2024-08-06 15:23:06 +0530

    initial commit

---
+++ src/main/java/com/example/threads_api_gateway/ThreadsApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.threads_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ThreadsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/threads_api_gateway/ThreadsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threads_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
